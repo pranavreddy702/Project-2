6e07f136273b5d0429bc00527dcfc731a438c7ed
pranav reddy <pranavreddy76@yahoo.co.in>
2016-11-02 16:23:07 -0400

    heroku

--- services/nutrition.js
+++ services/nutrition.js
@@ -8,7 +8,7 @@ const userInputItem = ''
 const itemFields = `fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat`
 
 function findFood(req, res, next) {
-  let userInputItem = req.body.users.foodInput
+  let userInputItem = req.body.foodInput
   fetch(`${APP_URL}${userInputItem}?${itemFields}&appId=${APP_ID}&appKey=${API_KEY}`)
   .then(r => r.json())
   .then((result) => {
